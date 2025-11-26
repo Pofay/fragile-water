a0b74281dd86512445afd608c56b840ac4fa4a98
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-11-26 11:41:37 +0800

    chore: added credo and credo configuration

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule FragileWater.MixProject do
   defp deps do
     [
       {:thousand_island, "~> 1.0"},
-      {:binary, "~> 0.0.5"}
+      {:binary, "~> 0.0.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
